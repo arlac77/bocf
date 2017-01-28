550875f8a6108ba2306ba05ca6760dbd62992460
Markus Felten <Markus.Felten@gmx.de>
2017-01-28 19:32:28 +0100

    wip

--- src/bocf.js
+++ src/bocf.js
@@ -8,6 +8,7 @@ const program = require('commander'),
   child_process = require('child_process'),
   mkdirp = require('mkdirp'),
   tar = require('tar-stream'),
+  tarfs = require('tar-fs'),
   pump = require('pump'),
   walk = require('walk');
 
@@ -25,6 +26,8 @@ program
   .option('-c, --config <file>', 'use config from file')
   .parse(process.argv);
 
+const out = fs.createWriteStream('/tmp/a.tar');
+
 expand(program.config ? "${include('" + path.basename(program.config) + "')}"
  : {}).then(config => {
 
@@ -35,33 +38,43 @@ expand(program.config ? "${include('" + path.basename(program.config) + "')}"
 
    const pack = tar.pack();
 
-   pump(pack,process.stderr);
+   pump(pack,out);
 
    pack.entry({ name: 'manifest' }, JSON.stringify(manifest));
 
+   for(const name of ['.gitignore','LICENSE','.npmignore']) {
+   fs.stat(name,(err,stat) => {
+     if(err) { console.log(err); return ; }
+     console.log(stat);
+     const entry = pack.entry({ name: name, size: stat.size }, (err) => {
+       console.log('end');
+       pack.finalize();
+     });
+     fs.createReadStream(name).pipe(entry);
+   });
+}
+
+/*
    const walker = walk.walk('./', {});
 
 
    walker.on('file', (root, fileStats, next) => {
 
      console.log(`got: ${fileStats.name}`);
-     const entry = pack.entry({ name: fileStats.name, size: fileStats.size }, err => {
-       console.log(`end: ${err}`);
 
-       if (err) return pack.destroy(err);
-       next();
-     }
-   );
+     const entry = pack.entry({ name: fileStats.name, size: fileStats.size },fs.readFileSync(fileStats.name));
 
-     const rs = fs.createReadStream(fileStats.name);
+     next();
+     //const rs = fs.createReadStream(fileStats.name);
 
-     pump(rs,entry);
+     //pump(rs,entry);
+     //setTimeout(() => next(),1000);
    });
 
    walker.on('end', () => {
      console.log("all done");
      pack.finalize();
-
    });
+*/
 
  });
