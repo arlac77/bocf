8bc5cf8e448cb74945689593f9dd8cbe77662ebc
Markus Felten <Markus.Felten@gmx.de>
2017-06-13 11:26:45 +0200

    wip

--- src/bocf.js
+++ src/bocf.js
@@ -1,6 +1,7 @@
-/* jslint node: true, esnext: true */
-
-'use strict';
+import {
+  expand
+}
+from 'config-expander';
 
 const program = require('caporal'),
   path = require('path'),
@@ -12,107 +13,119 @@ const program = require('caporal'),
   pump = require('pump'),
   walk = require('walk');
 
-import {
-  expand
-}
-from 'config-expander';
-
 
 program
-  .version(require(path.join(__dirname,'..','package.json')).version)
+  .version(require(path.join(__dirname, '..', 'package.json')).version)
   .description('build ocf image')
-  .action( (args, options, logger) => {
+  .action((args, options, logger) => {
     const out = fs.createWriteStream('/tmp/a.tar');
 
-    expand(options.config ? "${include('" + path.basename(options.config) + "')}"
-     : {}).then(config => {
+    expand(options.config ? "${include('" + path.basename(options.config) + "')}" : {}).then(config => {
 
-       const manifest = {
-         acKind: 'ImageManifest',
-         acVersion: '0.8.9'
-       };
+      const manifest = {
+        acKind: 'ImageManifest',
+        acVersion: '0.8.9'
+      };
 
-       const pack = tar.pack();
+      const pack = tar.pack();
 
-       pump(pack,out);
+      pump(pack, out);
 
-       pack.entry({ name: 'manifest' }, JSON.stringify(manifest));
+      pack.entry({
+        name: 'manifest'
+      }, JSON.stringify(manifest));
 
-       add(pack,'.');
-     });
+      add(pack, '.');
+    });
   })
   .option('-c, --config <file>', 'use config from file');
 
 program.parse(process.argv);
 
- function statAll(cwd = '.', entries = ['.']) {
-   const queue = entries;
+function statAll(cwd = '.', entries = ['.']) {
+  const queue = entries;
 
-   return callback => {
-     if (!queue.length) return callback();
-     let next = queue.shift();
-     let nextAbs = path.join(cwd, next);
+  return callback => {
+    if (!queue.length) {
+      return callback();
+    }
+    let next = queue.shift();
+    let nextAbs = path.join(cwd, next);
 
-     fs.stat(nextAbs, (err, stat) => {
-       if (err) return callback(err);
+    fs.stat(nextAbs, (err, stat) => {
+      if (err) {
+        return callback(err);
+      }
 
-       if (!stat.isDirectory()) return callback(null, next, stat);
+      if (!stat.isDirectory()) {
+        return callback(null, next, stat);
+      }
 
-       fs.readdir(nextAbs, (err, files) => {
-         if (err) return callback(err);
+      fs.readdir(nextAbs, (err, files) => {
+        if (err) {
+          return callback(err);
+        }
 
-         for (let i = 0; i < files.length; i++) {
-           queue.push(path.join(next, files[i]));
-         }
+        for (let i = 0; i < files.length; i++) {
+          queue.push(path.join(next, files[i]));
+        }
 
-         callback(null, next, stat);
-       });
-     });
-   };
- }
+        callback(null, next, stat);
+      });
+    });
+  };
+}
 
- function add(pack,cwd = '.') {
-   const statNext = statAll();
+function add(pack, cwd = '.') {
+  const statNext = statAll();
 
-   const onStat = (err, filename, stat) => {
-     if (err) return pack.destroy(err);
-     if (!filename) return pack.finalize();
+  const onStat = (err, filename, stat) => {
+    if (err) {
+      return pack.destroy(err);
+    }
+    if (!filename) {
+      return pack.finalize();
+    }
 
-     console.log(`onStat: ${filename}`);
-     const header = {
-       name: filename,
-       mtime: stat.mtime,
-       size: stat.size,
-       type: 'file',
-       uid: stat.uid,
-       gid: stat.gid
-     };
+    console.log(`onStat: ${filename}`);
+    const header = {
+      name: filename,
+      mtime: stat.mtime,
+      size: stat.size,
+      type: 'file',
+      uid: stat.uid,
+      gid: stat.gid
+    };
 
-     if (stat.isDirectory()) {
-       header.size = 0;
-       header.type = 'directory';
-       return pack.entry(header, onNextEntry);
-     }
+    if (stat.isDirectory()) {
+      header.size = 0;
+      header.type = 'directory';
+      return pack.entry(header, onNextEntry);
+    }
 
-     const entry = pack.entry(header, onNextEntry);
-     if (!entry) return;
+    const entry = pack.entry(header, onNextEntry);
+    if (!entry) {
+      return;
+    }
 
-/*
-     const rs = fs.createReadStream(path.join(cwd, filename));
+    /*
+         const rs = fs.createReadStream(path.join(cwd, filename));
 
-     rs.on('error', err => entry.destroy(err));
-     rs.on('end', () => console.log(`end ${filename}`));
+         rs.on('error', err => entry.destroy(err));
+         rs.on('end', () => console.log(`end ${filename}`));
 
-     pump(rs, entry);
-     */
-   };
+         pump(rs, entry);
+         */
+  };
 
-   const onNextEntry = err => {
-     //console.log(`onnextentry: ${err}`);
+  const onNextEntry = err => {
+    //console.log(`onnextentry: ${err}`);
 
-     if (err) return pack.destroy(err);
-     statNext(onStat);
-   };
+    if (err) {
+      return pack.destroy(err);
+    }
+    statNext(onStat);
+  };
 
-   onNextEntry();
+  onNextEntry();
 }
