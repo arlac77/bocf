54ef2283391b10261dfd8a9a279ced59dd2c1441
Markus Felten <Markus.Felten@gmx.de>
2017-11-27 15:51:53 +0100

    refactor: use rollup-plugin-json to provide package version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
+import json from 'rollup-plugin-json';
 
 export default {
   output: {
@@ -8,7 +9,7 @@ export default {
     format: 'cjs',
     banner: '#!/usr/bin/env node'
   },
-  plugins: [nodeResolve(), commonjs()],
+  plugins: [nodeResolve(), commonjs(), json()],
   external: ['config-expander'],
   input: pkg.module
 };
--- src/bocf-cli.js
+++ src/bocf-cli.js
@@ -1,12 +1,13 @@
 import { expand } from 'config-expander';
 import { archive, createManifest } from './archive';
+import { version } from '../package.json';
 
 const path = require('path');
 const fs = require('fs');
 const program = require('caporal');
 
 program
-  .version(require(path.join(__dirname, '..', 'package.json')).version)
+  .version(version)
   .description('build ocf image')
   .action(async (args, options) => {
     const out = fs.createWriteStream('/tmp/a.tar');
