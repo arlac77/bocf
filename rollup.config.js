679c99e0e3987c0a29b585e168a0e8593b69bd23
Markus Felten <Markus.Felten@gmx.de>
2018-09-21 21:43:30 +0200

    chore(rollup): nodeResolve -> resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,29 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import resolve from 'rollup-plugin-node-resolve';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import pkg from './package.json';
-import json from 'rollup-plugin-json';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
+import json from "rollup-plugin-json";
 
 export default {
   output: {
     file: pkg.bin.bocf,
-    format: 'cjs',
-    banner: '#!/usr/bin/env node',
+    format: "cjs",
+    banner: "#!/usr/bin/env node",
     interop: false
   },
-  plugins: [nodeResolve(), commonjs(), json()],
+  plugins: [resolve(), commonjs(), json()],
   external: [
-    'config-expander',
-    'fs',
-    'path',
-    'util',
-    'stream',
-    'buffer',
-    'events',
-    'constants',
-    'tar-stream',
-    'pump'
+    "config-expander",
+    "fs",
+    "path",
+    "util",
+    "stream",
+    "buffer",
+    "events",
+    "constants",
+    "tar-stream",
+    "pump"
   ],
   input: pkg.module
 };
