8a587cdad2cf7cba2d9e253cf2c6ad31fb19b9cf
Markus Felten <Markus.Felten@gmx.de>
2021-01-31 16:07:19 +0100

    fix: typos

--- src/bocf-cli.mjs
+++ src/bocf-cli.mjs
@@ -1,19 +1,26 @@
-import { expand } from 'config-expander';
-import { archive, createManifest } from './archive.mjs';
-import { basename } from 'path';
-import { createWriteStream } from 'fs';
+import { expand } from "config-expander";
+import { archive, createManifest } from "./archive.mjs";
+import { basename } from "path";
+import { createWriteStream, readFileSync } from "fs";
 import program from "commander";
 
+const { version, description } = JSON.parse(
+  readFileSync(new URL("../package.json", import.meta.url).pathname, {
+    encoding: "utf8"
+  })
+);
 
 program
-  .description('build ocf image')
-  .action(async (args, options) => {
-    const out = createWriteStream('/tmp/a.tar');
+  .usage(description)
+  .version(version)
+  .option("-c, --config <file>", "use config from file")
+  .action(async () => {
+    const options = program.opts();
+    const out = createWriteStream("/tmp/a.tar");
     const config = await expand(
       options.config ? "${include('" + basename(options.config) + "')}" : {}
     );
 
-    archive(out, '.', createManifest({ name: 'example.com/reduce-worker' }));
+    archive(out, ".", createManifest({ name: "example.com/reduce-worker" }));
   })
-  .option('-c, --config <file>', 'use config from file');
- .parse(process.argv);
+  .parse(process.argv);
